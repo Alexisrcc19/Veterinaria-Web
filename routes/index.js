832b01d7dabbf0473dc3e518a7d7a62c6c43b604
Daniel Patino <danielixo99@gmail.com>
2019-07-04 18:18:38 -0500

    implementacion de hbs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Veterinaria' });
+  res.render('index', { title: 'Veterinaria', lugar: 'principal' });
 });
 
 module.exports = router;
