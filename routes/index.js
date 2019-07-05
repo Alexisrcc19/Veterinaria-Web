2009ad111f0e02c5437455689a92d8a3909d9edc
Daniel Patino <danielixo99@gmail.com>
2019-07-04 19:26:03 -0500

    uso de hbs para cambio de ventanas (solo como ejemplo)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
-
+var hbs = require('handlebars');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Veterinaria', lugar: 'principal' });
+  res.render('index', { title: 'Veterinaria', principal: 'principal' });
+});
+router.get('/registro', function(req, res, next) {
+  res.render('index', { title: 'Veterinaria', mascota: 'mascota' });
 });
 
 module.exports = router;
