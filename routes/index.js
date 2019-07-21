17389cab8b3751298bb1c3a92ec3c7b0f6e97be5
Daniel Patino <danielixo99@gmail.com>
2019-07-21 13:52:29 -0500

    session y flash, con plantilla de registro

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var hbs = require('handlebars');
-var bd = require('../modelos/rol')
+var bd = require('../modelos/rol');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Veterinaria', principal: 'principal' });
+function verificar_inicio(req) {
+    return (req.session !== undefined && req.session.cuenta !== undefined);
+}
+var sacar = function (req, res, next) {
+    if (verificar_inicio(req)) {
+        next();
+    } else {
+        req.flash('error', 'Debes iniciar sesion primero!');
+        res.redirect('/');
+    }
+};
+router.get('/', function (req, res, next) {
+    if (req.session !== undefined && req.session.cuenta !== undefined ) {
+        res.render('index', {title: "Veterinaria",principal:'principal',  sesion: true});
+    } else {
+        res.render('index', {title: 'Medicos',inicio:'inicio', msg: {error: req.flash('error'), 
+                ok: req.flash('success')}});
+    }
 });
-router.get('/registro', function(req, res, next) {
-  res.render('index', { title: 'Veterinaria', mascota: 'mascota' });
+router.get('/registrarVeterinario', function(req, res, next) {
+  res.render('index', { title: 'Registrate', registrarVeterinario: 'registrarVeterinario' });
 });
 
 module.exports = router;
